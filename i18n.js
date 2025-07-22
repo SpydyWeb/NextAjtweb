bfa39b659fae6883a46d5f7612cfecc7902feac2
spydy <70287710+SpydyWeb@users.noreply.github.com>
2025-07-22 14:46:15 +0000

    changes

--- i18n.js
+++ i18n.js
@@ -1,100 +1,77 @@
+"use client";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import landingPageEN from "@/public/locales/translation/landingpage/en.json";
+import landingPageAR from "@/public/locales/translation/landingpage/ar.json";
+import motorPlanEN from "@/public/locales/translation/individualproducts/motorplan/en.json";
+import motorPlanAR from "@/public/locales/translation/individualproducts/motorplan/ar.json";
+import travelPlanEN from "@/public/locales/translation/individualproducts/travelplan/en.json";
+import travelPlanAR from "@/public/locales/translation/individualproducts/travelplan/ar.json";
+import visitVisaPlanEN from "@/public/locales/translation/individualproducts/visitvisaplan/en.json";
+import visitVisaPlanAR from "@/public/locales/translation/individualproducts/visitvisaplan/ar.json";
+import houseHolderPlanAR from "@/public/locales/translation/individualproducts/householder/ar.json";
+import houseHolderPlanEN from "@/public/locales/translation/individualproducts/householder/en.json";
+import aboutUsEN from "@/public/locales/translation/aboutus/en.json";
+import aboutUsAR from "@/public/locales/translation/aboutus/ar.json";
+import pnsAR from "@/public/locales/translation/pns/ar.json";
+import pnsEN from "@/public/locales/translation/pns/en.json";
+import consentFormEN from "@/public/locales/translation/ConsentForm/En.json";
+import consentFormAR from "@/public/locales/translation/ConsentForm/En.json";
+import contactEN from "@/public/locales/translation//Contact/En.json";
+import contactAR from "@/public/locales/translation/Contact/Ar.json";
+import privacyPolicyEN from "@/public/locales/translation/Privacypolicy/En.json";
+import privacyPolicyAR from "@/public/locales/translation/Privacypolicy/Ar.json";
+import shareHolderEN from "@/public/locales/translation/ShareHolder/En.json";
+import shareHolderAR from "@/public/locales/translation/ShareHolder/Ar.json";
+import PNSFlowEN from "@/public/locales/translation/PNSFlow/en.json";
+import PNSFlowAR from "@/public/locales/translation/PNSFlow/ar.json";
+import EServiceEN from "@/public/locales/translation/EServices/En.json";
+import EServiceAR from "@/public/locales/translation/EServices/Ar.json";
+import DownlaodEN from "@/public/locales/translation/Download/En.json";
+import DownlaodAR from "@/public/locales/translation/Download/Ar.json";
+import CrafstmanEn from "@/public/locales/translation/individualproducts/Crafstman/en.json";
+import CrafstmanAr from "@/public/locales/translation/individualproducts/Crafstman/ar.json";
+import FraudEN from "@/public/locales/translation/fraud/en.json";
+import FraudAR from "@/public/locales/translation/fraud/ar.json";
+import footerEN from "@/public/locales/translation/FooterLong/en.json";
+import footerAR from "@/public/locales/translation/FooterLong/ar.json";
+import FAQsEN from "@/public/locales/translation/FAQs/En.json";
+import FAQsAR from "@/public/locales/translation/FAQs/Ar.json";
+import EnquiryEN from "@/public/locales/translation/Enquiry/En.json";
+import EnquiryAR from "@/public/locales/translation/Enquiry/Ar.json";
+import MotorSearchEn from "@/public/locales/translation/PrintPolicy/En.json";
+import MotorSearchAr from "@/public/locales/translation/PrintPolicy/Ar.json";
+import MedicalMalpractisePlanEn from "@/public/locales/translation/individualproducts/MedicalMalpractisePlan/En.json";
+import MedicalMalpractisePlanAr from "@/public/locales/translation/individualproducts/MedicalMalpractisePlan/Ar.json";
+import MedicalDomesticHelperEn from "@/public/locales/translation/individualproducts/MedicalDomesticHelper/En.json";
+import MedicalDomesticHelperAr from "@/public/locales/translation/individualproducts/MedicalDomesticHelper/Ar.json";
+import MedicalAR from "@/public/locales/translation/corporateproducts/medical/ar.json";
+import MedicalEN from "@/public/locales/translation/corporateproducts/medical/en.json";
+import MarineEN from "@/public/locales/translation/corporateproducts/marine/en.json";
+import MarineAR from "@/public/locales/translation/corporateproducts/marine/ar.json";
+import AccidentAR from "@/public/locales/translation/corporateproducts/accident/ar.json";
+import AccidentEN from "@/public/locales/translation/corporateproducts/accident/en.json";
+import PropertyPlanEn from "@/public/locales/translation/corporateproduct/PropertyPlan/En.json";
+import PropertyPlanAr from "@/public/locales/translation/corporateproduct/PropertyPlan/Ar.json";
+import CorporatePnSEn from "@/public/locales/translation/corporateproduct/PandS/En.json";
+import CorporatePnSAr from "@/public/locales/translation/corporateproduct/PandS/Ar.json";
+import AviationEn from "@/public/locales/translation/corporateproduct/Aviation/En.json";
+import AviationAr from "@/public/locales/translation/corporateproduct/Aviation/Ar.json";
+import engineeringEn from "@/public/locales/translation/corporateproduct/engineering/En.json";
+import engineeringAr from "@/public/locales/translation/corporateproduct/engineering/Ar.json";
+import PSClaimInstructionEn from "@/public/locales/translation/EServices/PSClaimInstruction/En.json";
+import PSClaimInstructionAr from "@/public/locales/translation/EServices/PSClaimInstruction/Ar.json";
+import MotorClaimInsEn from "@/public/locales/translation/EServices/MotorClaiminstruction/En.json";
+import MotorClaimInsAr from "@/public/locales/translation/EServices/MotorClaiminstruction/Ar.json";
+import MMPClaimEn from "@/public/locales/translation/EServices/MMPClaimInstruction/En.json";
+import MMPClaimAr from "@/public/locales/translation/EServices/MMPClaimInstruction/Ar.json";
+import TravelClaimInstructionEn from "@/public/locales/translation/EServices/TravelClaimInstruction/En.json";
+import TravelClaimInstructionAr from "@/public/locales/translation/EServices/TravelClaimInstruction/Ar.json";
+import ServiceRequestEn from "@/public/locales/translation/ServiceRequests/En.json";
+import ServiceRequestAr from "@/public/locales/translation/ServiceRequests/Ar.json";
+import MotorflowEn from "@/public/locales/translation/MotorFlow/En.json";
+import MotorflowAr from "@/public/locales/translation/MotorFlow/Ar.json";
 
-import landingPageEN from './constants/translation/landingpage/en.json';
-import landingPageAR from './constants/translation/landingpage/ar.json';
-import motorPlanEN from './constants/translation/individualproducts/motorplan/en.json';
-import motorPlanAR from './constants/translation/individualproducts/motorplan/ar.json';
-import travelPlanEN from './constants/translation/individualproducts/travelplan/en.json';
-import travelPlanAR from './constants/translation/individualproducts/travelplan/ar.json';
-import visitVisaPlanEN from './constants/translation/individualproducts/visitvisaplan/en.json';
-import visitVisaPlanAR from './constants/translation/individualproducts/visitvisaplan/ar.json';
-import houseHolderPlanAR from './constants/translation/individualproducts/householder/ar.json';
-import houseHolderPlanEN from './constants/translation/individualproducts/householder/en.json';
-import aboutUsEN from './constants/translation/aboutus/en.json';
-import aboutUsAR from './constants/translation/aboutus/ar.json';
-import pnsAR from './constants/translation/pns/ar.json';
-import pnsEN from './constants/translation/pns/en.json';
-import consentFormEN from './constants/translation/ConsentForm/En.json';
-import consentFormAR from './constants/translation/ConsentForm/En.json';
-import contactEN from './constants/translation//Contact/En.json';
-import contactAR from './constants/translation/Contact/Ar.json';
-import privacyPolicyEN from './constants/translation/Privacypolicy/En.json';
-import privacyPolicyAR from './constants/translation/Privacypolicy/Ar.json';
-import shareHolderEN from './constants/translation/ShareHolder/En.json';
-import shareHolderAR from './constants/translation/ShareHolder/Ar.json';
-import PNSFlowEN from './constants/translation/PNSFlow/en.json';
-import PNSFlowAR from './constants/translation/PNSFlow/ar.json';
-import EServiceEN from './constants/translation/EServices/En.json';
-import EServiceAR from './constants/translation/EServices/Ar.json';
-import DownlaodEN from './constants/translation/Download/En.json';
-import DownlaodAR from './constants/translation/Download/Ar.json';
-import CrafstmanEn from './constants/translation/individualproducts/Crafstman/en.json';
-import CrafstmanAr from './constants/translation/individualproducts/Crafstman/ar.json';
-import FraudEN from './constants/translation/fraud/en.json';
-import FraudAR from './constants/translation/fraud/ar.json';
-import footerEN from "./constants/translation/FooterLong/en.json";
-import footerAR from "./constants/translation/FooterLong/ar.json";
-import FAQsEN from "./constants/translation/FAQs/En.json";
-import FAQsAR from "./constants/translation/FAQs/Ar.json";
-import EnquiryEN from "./constants/translation/Enquiry/En.json";
-import EnquiryAR from "./constants/translation/Enquiry/Ar.json";
-import MotorSearchEn from './constants/translation/PrintPolicy/En.json'
-import MotorSearchAr from './constants/translation/PrintPolicy/Ar.json'
-import MedicalMalpractisePlanEn from './constants/translation/individualproducts/MedicalMalpractisePlan/En.json';
-import MedicalMalpractisePlanAr from './constants/translation/individualproducts/MedicalMalpractisePlan/Ar.json';
-import MedicalDomesticHelperEn from './constants/translation/individualproducts/MedicalDomesticHelper/En.json';
-import MedicalDomesticHelperAr from './constants/translation/individualproducts/MedicalDomesticHelper/Ar.json';
-import MedicalAR from "./constants/translation/corporateproducts/medical/ar.json";
-import MedicalEN from "./constants/translation/corporateproducts/medical/en.json";
-import MarineEN from "./constants/translation/corporateproducts/marine/en.json";
-import MarineAR from "./constants/translation/corporateproducts/marine/ar.json";
-import AccidentAR from "./constants/translation/corporateproducts/accident/ar.json";
-import AccidentEN from "./constants/translation/corporateproducts/accident/en.json";
-import PropertyPlanEn from './constants/translation/corporateproduct/PropertyPlan/En.json'
-import PropertyPlanAr from './constants/translation/corporateproduct/PropertyPlan/Ar.json'
-import CorporatePnSEn from './constants/translation/corporateproduct/PandS/En.json'
-import CorporatePnSAr from './constants/translation/corporateproduct/PandS/Ar.json'
-import AviationEn from './constants/translation/corporateproduct/Aviation/En.json'
-import AviationAr from './constants/translation/corporateproduct/Aviation/Ar.json'
-import engineeringEn from './constants/translation/corporateproduct/engineering/En.json'
-import engineeringAr from './constants/translation/corporateproduct/engineering/Ar.json'
-import PSClaimInstructionEn from './constants/translation/EServices/PSClaimInstruction/En.json'
-import PSClaimInstructionAr from './constants/translation/EServices/PSClaimInstruction/Ar.json'
-import MotorClaimInsEn from './constants/translation/EServices/MotorClaiminstruction/En.json'
-import MotorClaimInsAr from './constants/translation/EServices/MotorClaiminstruction/Ar.json'
-import MMPClaimEn from  './constants/translation/EServices/MMPClaimInstruction/En.json'
-import MMPClaimAr from  './constants/translation/EServices/MMPClaimInstruction/Ar.json'
-import TravelClaimInstructionEn from './constants/translation/EServices/TravelClaimInstruction/En.json'
-import TravelClaimInstructionAr from './constants/translation/EServices/TravelClaimInstruction/Ar.json'
-import ServiceRequestEn from './constants/translation/ServiceRequests/En.json'
-import ServiceRequestAr from './constants/translation/ServiceRequests/Ar.json'
-
-import MotorflowEn from './constants/translation/MotorFlow/En.json'
-import MotorflowAr from './constants/translation/MotorFlow/Ar.json'
-//mahesh transalations
-import FooternewEn from './components/Home/Newcomponenets/ModalPopups.tsx/Footervalues/en.json'
-import FooternewAR from './components/Home/Newcomponenets/ModalPopups.tsx/Footervalues/ar.json'
-import AboutsValuesEn from './components/Home/Newcomponenets/ModalPopups.tsx/Aboutusvalues/en.json'
-import AboutsValuesAR from './components/Home/Newcomponenets/ModalPopups.tsx/Aboutusvalues/ar.json'
-import LoginvaluesEn from './components/Home/Newcomponenets/ModalPopups.tsx/Loginvalues/en.json'
-import LoginvaluesAR from './components/Home/Newcomponenets/ModalPopups.tsx/Loginvalues/ar.json'
-import OtpVerificationEn from './components/Home/Newcomponenets/ModalPopups.tsx/OtpVerification/en.json'
-import OtpVerificationAR from './components/Home/Newcomponenets/ModalPopups.tsx/OtpVerification/ar.json'
-import VerifyvaluesEn from './components/Home/Newcomponenets/ModalPopups.tsx/Verifyvalues/en.json'
-import VerifyvaluesAR from './components/Home/Newcomponenets/ModalPopups.tsx/Verifyvalues/ar.json'
-import CreateaccountsEn from './components/Home/Newcomponenets/ModalPopups.tsx/Createaccounts/en.json'
-import CreateaccountsAR from './components/Home/Newcomponenets/ModalPopups.tsx/Createaccounts/ar.json'
-import CorporateInsuranceEn from './components/Home/Newcomponenets/ModalPopups.tsx/Corporatevalues/en.json'
-import CorporateInsuranceAR from './components/Home/Newcomponenets/ModalPopups.tsx/Corporatevalues/ar.json'
-import IndividualInsuranceEn from './components/Home/Newcomponenets/ModalPopups.tsx/Individualvalues/en.json'
-import IndividualInsuranceAR from './components/Home/Newcomponenets/ModalPopups.tsx/Individualvalues/ar.json'
-import FirstPageEn from './components/Home/Newcomponenets/ModalPopups.tsx/Firstpagevalues/en.json'
-import FirstPageAR from './components/Home/Newcomponenets/ModalPopups.tsx/Firstpagevalues/ar.json'
-import HeadervaluesEn from './components/Home/Newcomponenets/ModalPopups.tsx/Headervalues/en.json'
-import HeadervaluesAR from './components/Home/Newcomponenets/ModalPopups.tsx/Headervalues/ar.json'
-import MyProfilevaluesEn from './components/Home/Newcomponenets/ModalPopups.tsx/MyProfilevalues/en.json'
-import MyProfilevaluesAR from './components/Home/Newcomponenets/ModalPopups.tsx/MyProfilevalues/ar.json'
 const resources = {
   en: {
     translation: {
@@ -133,17 +110,6 @@ const resources = {
       ...MarineEN,
       ...MedicalEN,
       ...MotorflowEn,
-      ...FooternewEn,
-      ...AboutsValuesEn,
-      ...LoginvaluesEn,
-      ...OtpVerificationEn,
-      ...VerifyvaluesEn,
-      ...CreateaccountsEn,
-      ...CorporateInsuranceEn,
-      ...IndividualInsuranceEn,
-      ...FirstPageEn,
-      ...HeadervaluesEn,
-      ...MyProfilevaluesEn
     },
   },
   ar: {
@@ -183,27 +149,17 @@ const resources = {
       ...MarineAR,
       ...MedicalAR,
       ...MotorflowAr,
-      ...FooternewAR,
-      ...AboutsValuesAR,
-      ...LoginvaluesAR,
-      ...OtpVerificationAR,
-      ...VerifyvaluesAR,
-      ...CreateaccountsAR,
-      ...CorporateInsuranceAR,
-       ...IndividualInsuranceAR,
-       ...FirstPageAR,
-       ...HeadervaluesAR,
-       ...MyProfilevaluesAR
     },
   },
 };
-i18n
-  .use(initReactI18next).init({
-    resources,
-    lng: 'en',
-    keySeparator: false,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 export default i18n;
--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,32 +1,31 @@
-
-// lib/i18n.ts
-import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next';
+"use client";
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
-    supportedLngs: ['en', 'ar'],
+    fallbackLng: "en",
+    supportedLngs: ["en", "ar"],
     debug: false,
-    
+
     backend: {
       // Correct path for Next.js public directory
-      loadPath: '/public/constants/translation/{{ns}}/{{lng}}.json',
+      loadPath: "/public/constants/translation/{{ns}}/{{lng}}.json",
     },
-    
+
     interpolation: {
       escapeValue: false,
     },
-    
+
     react: {
       useSuspense: false,
     },
-    
+
     ns: [], // Don't preload
     defaultNS: undefined,
   });
