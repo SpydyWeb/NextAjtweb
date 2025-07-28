94ee684b7299c6b55b14ed79e464c90f289436d4
spydyweb <ankitkumar65438@gmail.com>
2025-07-28 11:18:32 +0530

    donne

--- app/I18nProvider.tsx
+++ app/I18nProvider.tsx
@@ -1,7 +1,7 @@
 // app/i18n-provider.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "@/lib/i18n";
 
@@ -11,6 +11,13 @@ export function I18nProvider({ children }: { children: React.ReactNode }) {
       ? "ltr"
       : "rtl"
   );
+  useEffect(() => {
+    setdirection(
+      typeof window !== "undefined" && localStorage.getItem("lang") === "ar"
+        ? "ltr"
+        : "rtl"
+    );
+  }, [localStorage.getItem("lang")]);
   return (
     <I18nextProvider i18n={i18n}>
       <div dir={direction}>{children}</div>
