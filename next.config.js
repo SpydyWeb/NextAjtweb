9a11dd194dcd8c1849368bfb974de5d154db137d
spydyweb <ankitkumar65438@gmail.com>
2025-07-30 19:07:26 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-// next.config.js - If you want to use standalone mode
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
-  output: "standalone",
-  poweredByHeader: false,
+  images: { unoptimized: true },  
+  poweredByHeader: false
 };
 
 module.exports = nextConfig;
